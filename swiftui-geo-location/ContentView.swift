70a1e10b2d6a527cab7d38c3d3d48f372ec4c41e
yorifuji <yorifuji@gmail.com>
2021-05-16 16:02:56 +0900

    Add Project

---
+++ swiftui-geo-location/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  swiftui-geo-location
+//
+//  Created by yorifuji on 2021/05/16.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ swiftui-geo-location/swiftui_geo_locationApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftui_geo_locationApp.swift
+//  swiftui-geo-location
+//
+//  Created by yorifuji on 2021/05/16.
+//
+
+import SwiftUI
+
+@main
+struct swiftui_geo_locationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
